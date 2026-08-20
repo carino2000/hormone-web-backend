130ea47e95820959b4c3728b249b87a6f76c01e1
BaeJihoon <carino2000@naver.com>
2026-08-20 11:11:29 +0900

    Initial commit

---
+++ src/main/java/com/sixletter/hormone_web_backend/HormoneWebBackendApplication.java
@@ -0,0 +1,13 @@
+package com.sixletter.hormone_web_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HormoneWebBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HormoneWebBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sixletter/hormone_web_backend/HormoneWebBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sixletter.hormone_web_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HormoneWebBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
